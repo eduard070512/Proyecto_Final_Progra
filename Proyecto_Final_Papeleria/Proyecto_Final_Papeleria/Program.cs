659d10872cf8eea27536768af082a7d3ec4b67c1
eduard070512 <michelmm2007@gmail.com>
2024-11-02 19:30:21 -0600

    LOGIN
    cambio de nombre a login al from principal

--- Proyecto_Final_Papeleria/Proyecto_Final_Papeleria/Program.cs
+++ Proyecto_Final_Papeleria/Proyecto_Final_Papeleria/Program.cs
@@ -16,7 +16,7 @@ namespace Proyecto_Final_Papeleria
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Principal());
+            Application.Run(new Login());
         }
     }
 }
